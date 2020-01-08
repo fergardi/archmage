eda3e8b8c4cd17277823e58057945ccfe2a64c44
Fergardi <fergardi@gmail.com>
2020-01-08 23:31:24 +0100

    Added i18n. Improved landing. Added assets.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { LandingComponent } from './landing/landing.component';
+
+// AoT requires an exported function for factories
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -14,7 +22,15 @@ import { LandingComponent } from './landing/landing.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (createTranslateLoader),
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -6,10 +6,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing.component.scss']
 })
 export class LandingComponent implements OnInit {
+  // global
+  public factions = []
 
   constructor() { }
 
   ngOnInit() {
+    this.factions = [
+      {
+        name: 'Sacro',
+        color: 'yellow',
+        description: 'descripcion',
+        image: 'holy.png'
+      },
+      {
+        name: 'Muerto',
+        color: 'purple',
+        description: 'descripcion',
+        image: 'undead.png'
+      },
+      {
+        name: 'Caos',
+        color: 'red',
+        description: 'descripcion',
+        image: 'chaos.png'
+      },
+      {
+        name: 'Natural',
+        color: 'green',
+        description: 'descripcion',
+        image: 'nature.png'
+      },
+      {
+        name: 'Mental',
+        color: 'blue',
+        description: 'descripcion',
+        image: 'mental.png'
+      }
+    ]
   }
 
 }
